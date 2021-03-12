57c5bfe7476c2cfcbb293774c87eefe5f72da609
pratikinamake <36621212+pratikinamake@users.noreply.github.com>
2021-03-12 14:53:19 +0530

    Update variable.tf
    new code

--- variable.tf
+++ variable.tf
@@ -1,13 +1,14 @@
 
+
 variable "vpc_cidr" {
-  default = "10.10.0.0/21"
+  default = "10.10.0.0/22"
 }
 
 variable "subnet_cidrs_public" {
   description = "Subnet CIDRs for public subnets (length must match configured availability_zones)"
   # this could be further simplified / computed using cidrsubnet() etc.
   # https://www.terraform.io/docs/configuration/interpolation.html#cidrsubnet-iprange-newbits-netnum-
-  default = ["10.10.0.0/24", "10.10.1.0/24"]
+  default = ["10.10.0.0/25", "10.10.1.0/25"]
   type    = "list"
 }
 
@@ -16,7 +17,7 @@ variable "subnet_cidrs_private" {
   description = "Subnet CIDRs for public subnets (length must match configured availability_zones)"
   # this could be further simplified / computed using cidrsubnet() etc.
   # https://www.terraform.io/docs/configuration/interpolation.html#cidrsubnet-iprange-newbits-netnum-
-  default = ["10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+  default = ["10.10.2.0/25", "10.10.2.128/25", "10.10.3.0/25", "10.10.3.128/25"]
   type    = "list"
 }
 
